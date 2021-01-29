46cd08f1312389130b6745169cbbc608f6c1eba1
Your Name <you@example.com>
2021-01-29 11:07:34 -0600

    2 - Login Token POST Working 200

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,62 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  // const { push } = useHistory();
+  const [formValues, setFormValues] = useState({
+      username:'', 
+      password: '',
+  });
+
+  const handleChanges = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+        ... formValues,
+        [name]: value,
+    });
+};
+
+  const handlelogin = (e) => {
+    const config = {
+      baseURL: "http://localhost:5000",
+    };
+    e.preventDefault();
+    axiosWithAuth(config)
+      .post("/api/login", formValues)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        // change this berlw
+        // push("/friends");   
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-      <h1>
-        Welcome to the Bubble App!
-        <p>Build a login page here</p>
-      </h1>
+      <div>
+      <form onSubmit={handlelogin}>
+        <label htmlFor="username">username</label>
+        <input
+          id="username"
+          value={formValues.username}
+          name="username"
+          onChange={handleChanges}
+        />
+        <label htmlFor="password">password</label>
+        <input
+          id="password"
+          value={formValues.password}
+          type="password"
+          name="password"
+          onChange={handleChanges}
+        />
+        <button>Login</button>
+      </form>
+    </div>
     </>
   );
 };
@@ -21,5 +67,10 @@ export default Login;
 //1. Build a form containing a username and password field.
 //2. Add whatever state nessiary for form functioning.
 //3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEST "username" and "password" RESPECTIVELY.
-//4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
+//4. 
+//4b.MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT “username” and “password” RESPECTIVELY.
 //5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
+
+
+
+// XXXIf either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -1,4 +1,13 @@
 import axios from "axios";
 
+const defaultConfig = {
+  headers: { authorization: localStorage.getItem("token") },
+  baseURL: "http://localhost:5000",
+};
+
+export const axiosWithAuth = (config = defaultConfig) => {
+  return axios.create(config);
+};
+
 //Task List:
 //Build and export a function used to send in our authorization token
